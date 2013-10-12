0eef5420f3354a8d64eb73194e2b6e45cfd85de0
Daron Yondem <daron@yondem.com>
2013-10-12 12:17:49 +0300

    Initial API Wrapper Setup Complete.

---
+++ NikePlusAPI/APIClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NikePlusAPI
+{
+    public class APIClient
+    {
+        public APIClient(string valid_token = "")
+        {
+            token = valid_token;
+        }
+
+        private string token;
+        public string Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+        
+        private HttpClient aClient = new HttpClient();
+        public HttpClient Client
+        {
+            get
+            {
+                aClient.DefaultRequestHeaders.Clear();
+                if (!string.IsNullOrEmpty(token))
+                {                    
+                    aClient.DefaultRequestHeaders.Add("access_token", token);
+                }
+                return aClient;
+            }
+        }
+    }
+}
